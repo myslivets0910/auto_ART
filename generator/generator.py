8759f6e58dbe3e34834085c25236d64cb505949f
pavlik_masl_QA <myslivets0910@gmail.com>
2025-06-13 19:08:36 +0300

    add data.py web_table/ add generator.py web_table

--- generator/generator.py
+++ generator/generator.py
@@ -1,5 +1,5 @@
 # устанавливаем пакет для генерации данных + заводим данные которые надо сгенерировать
-
+import random
 
 from data.data import Person
 from faker import Faker
@@ -10,7 +10,12 @@ Faker.seed()
 def generated_person(): # заводим данные которые надо сгенерировать
     yield Person(
         full_name=faker_ru.first_name() + " " + faker_ru.last_name() + " " + faker_ru.middle_name(),
-        email= faker_ru.email(),
+        firstname=faker_ru.first_name(),
+        lastname=faker_ru.last_name(),
+        age=random.randint(10,80),
+        salary=random.randint(3000,10000),
+        department=faker_ru.job() ,
+        email=faker_ru.email(),
         current_address= faker_ru.address(),
         permanent_address=faker_ru.address(),
     )
